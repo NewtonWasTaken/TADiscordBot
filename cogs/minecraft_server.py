96b10d02ce90e6151ecb6c26913a0d676eed7cb2
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2021-10-08 21:20:11 +0200

    changed names for minecraft server

--- cogs/minecraft_server.py
+++ cogs/minecraft_server.py
@@ -9,44 +9,43 @@ class Minecraft(commands.Cog):
 
     @commands.group(name='server', invoke_without_command=True, help='Zobrazí IP a verzi našeho serveru', usage='!server <status> \nstatus: nepovinný, ukáže jestli je server online')
     async def server(self, ctx):
-        await ctx.send('IP našeho serveru je: TercieA.minehut.gg, Je na verzi 1.16.5')
+        await ctx.send('IP našeho serveru je: BigyKvarta.minehut.gg, Je na verzi 1.17.1')
 
     @server.command(name='status')
     async def status_subcommand(self, ctx):
-        response = requests.get('https://api.minehut.com/server/TercieA?byName=true')
+        response = requests.get('https://api.minehut.com/server/6037e2a8800860017c1f0ee0')
         server = response.json()
         count = server['server']['playerCount']
         maxplayers = server['server']['maxPlayers']
+        print(server)
         if server['server']['online'] == False:
             embed = discord.Embed(title="Server je offline!", color=0xff0000)
-            embed.set_author(name="Minecraft Server Tercie A")
+            embed.set_author(name="Minecraft Server Kvarta A")
             embed.set_thumbnail(
                 url=self.client.user.avatar_url)
             embed.add_field(name="Připoj se na server a do chatu napiš:",
-                            value="/join TercieA, pak se odpoj a počkej až se server zapne", inline=False)
-            embed.set_footer(text="IP adresa serveru: TercieA.minehut.gg")
+                            value="/join BigyKvarta, pak se odpoj a počkej až se server zapne", inline=False)
+            embed.set_footer(text="IP adresa serveru: BigyKvarta.minehut.gg")
             await ctx.send(embed=embed)
         elif server['server']['online'] == True:
             embed = discord.Embed(title="Server je online!", color=0x1fd312)
-            embed.set_author(name="Minecraft Server Tercie A")
+            embed.set_author(name="Minecraft Server BigyKvarta")
             embed.set_thumbnail(
                 url=self.client.user.avatar_url)
             embed.add_field(name="Počet lidí na serveru:",
                             value=f'{count} z {maxplayers} \n' + count * ' :blue_square: ' + (
                                         10 - count) * ' :white_large_square: ', inline=False)
-            embed.set_footer(text="IP adresa serveru: TercieA.minehut.gg")
+            embed.set_footer(text="IP adresa serveru: BigyKvarta.minehut.gg")
             await ctx.send(embed=embed)
 
-
+    '''
     @server.command(name = 'start', invoke_without_command=True)
     async def start_server_subcommand(self, ctx):
-        data = {'authorization':'d1c72c06-0375-4951-96a8-25cd008e223b', 'x-session-id':'b5c0d2be-0a9f-4c1f-95be-5994d2ee2068'}
-        s = requests.get('https://api.minehut.com/users/ghost_login', data = data)
-
-        r = requests.post('https://api.minehut.com/server/6037e2a8800860017c1f0ee0/start_service')
-        print(r.text)
-        print(s.text)
+        s = requests.get('https://api.minehut.com/users/login', data = data)
+        r = requests.post('https://api.minehut.com/server/6037e2a8800860017c1f0ee0/start')
+        print(r)
         await ctx.send('server se zapíná i guess')
+        '''
 def setup(client):
     client.add_cog(Minecraft(client))
 
