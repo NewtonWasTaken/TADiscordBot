7de589bb7b89b3f1e1a11666c62d5de0527314ea
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-04-19 22:28:55 +0200

    added give alias to command sell in economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -248,7 +248,7 @@ class Ekonomika(commands.Cog):
             else:
                 await ctx.send(f'Nemáš dost {Callouts().emote} na tuto platbu xd. Poor')
 
-    @commands.command(help='Prodáš item. Jméno se uvádí podle jména emotu.', usage='!sell [zvíře] (počet-nepovinný)')
+    @commands.command(help='Prodáš item. Jméno se uvádí podle jména emotu.', usage='!sell [zvíře] (počet-nepovinný)', aliases=['give'])
     async def sell(self, ctx, item, count=None):
         user = ctx.author
         server = ctx.guild
