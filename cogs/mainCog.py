343ec08e1208e2bc8a76f6f2c4f49d9f70649205
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-09-14 21:48:29 +0200

    Updated to discord.py 2.0. Only main.py and mainCog.py are working right now. Still needs to adjust.

--- cogs/mainCog.py
+++ cogs/mainCog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from google_trans_new import google_translator
+from discord import app_commands
 from discord.utils import get
 import requests
 import os
@@ -10,33 +11,47 @@ translator = google_translator()
 class Funkce(commands.Cog):
     def __init__(self, client):
         self.client = client
-    @commands.command(help='Nikdy nevíš', usage='!shrug')
-    async def shrug(self, ctx):
-        await ctx.send('¯\_(ツ)_/¯')
 
-    @commands.command(help='Nezobrazí ping bota', usage='!ping')
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
+    @app_commands.command()
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def test(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Hello from private command!")
 
-    @commands.command(help='to je opravdu yum', usage='!yum')
-    async def yum(self, ctx):
-        await ctx.send('yim yum')
 
-    @commands.command(help='Zobrazí ping bota', usage='!pong')
-    async def pong(self, ctx):
-        await ctx.send(f'Nemyslel jsi ping? {round(self.client.latency * 1000)}ms')
+    @app_commands.command(name="shrug",description='Nikdy nevíš')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def shrug(self, interaction):
+        await interaction.response.send_message('¯\_(ツ)_/¯')
 
-    @commands.command(help='Zavolá bajs na kompet', usage='!kompet')
-    async def kompet(self, ctx):
-        await ctx.send(
+    @app_commands.command(description='Nezobrazí ping bota')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def ping(self, interaction):
+        await interaction.response.send_message('Pong!')
+
+    @app_commands.command(description='to je opravdu yum')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def yum(self, interaction):
+        await interaction.response.send_message('yim yum')
+
+    @app_commands.command(description='Zobrazí ping bota')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def pong(self, interaction):
+        await interaction.response.send_message(f'Nemyslel jsi ping? {round(self.client.latency * 1000)}ms')
+
+    @app_commands.command(description='Zavolá bajs na kompet')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def kompet(self, interaction):
+        await interaction.response.send_message(
             '<@621353544490024961> , <@551426822299189259>, <@621701863821148166>, <@558659043833675806> pojďte hrát!')
 
-    @commands.command(help=f'Ukáže {Callouts().name} Coin zblízka', usage='!emote')
-    async def emote(self, ctx):
-        await ctx.send(f'{Callouts().emote}')
+    @app_commands.command(description=f'Ukáže {Callouts().name} Coin zblízka')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def emote(self, interaction):
+        await interaction.response.send_message(f'{Callouts().emote}')
 
-    @commands.command(help='Kdo vyrobil bota?',usage='!credit')
-    async def credit(self, ctx):
+    @app_commands.command(description='Kdo vyrobil bota?')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def credit(self, interaction):
         embed = discord.Embed(
             title="Credits",
             description=
@@ -44,64 +59,14 @@ class Funkce(commands.Cog):
             color=0x14db3c)
         embed.set_thumbnail(
             url=self.client.user.avatar_url)
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command(help='Přeloží zadaný text z EN do CZ', usage='!translate [text]')
-    async def translate(self, ctx):
-        await ctx.send('Překladač je dočasně nedostupný...')
-
-    @commands.command(help='Zobrazí help na všechny commandy', usage='!help (command-nepovinný)')
-    async def help(self, ctx, command=None):
-            command_names_list = [x.name for x in self.client.commands]
-            if command in command_names_list:
-                help = self.client.get_command(command).help
-                usage = self.client.get_command(command).usage
-                embed = discord.Embed(title=f"Help !{command}", description=f"Tady je help pro command {command}:", color=0xff0000)
-                embed.set_author(name=f"{Callouts().name} Discord Bot")
-                embed.set_thumbnail(
-                    url=self.client.user.avatar_url)
-                embed.add_field(name="Použití:", value=f"`{usage}`", inline=False)
-                embed.add_field(name="Co dělá?", value=f"`{help}`", inline=True)
-                embed.set_footer(text="Pro help s jakýmkoli commandem napiš !help [command]")
-                await ctx.send(embed=embed)
-            elif command == None:
-                embed = discord.Embed(
-                    title="Help",
-                    description=
-                    "Pokud potřebuješ s čimkoli pomoct, tady jsou všechny příkazy:",
-                    color=0xff0000)
-                embed.set_author(name=f"{Callouts().name} Discord Bot")
-                embed.set_thumbnail(
-                    url=
-                    self.client.user.avatar_url
-                )
-                embed.add_field(name="Funkce :gear: ", value="`ping` `pong` `status` `mute` `unmute` `clear` `kick` `ban` `blacklist`", inline=False)
-                embed.add_field(name=f"Vlastní commandy {Callouts().emote} ",
-                                value="`yum` `emote` `kompet` `credit` `shrug` `translate`", inline=False)
-                embed.add_field(name="Random věci :slot_machine: ",
-                                value="`ucitel` `howgay` `simp` `rng` `meme` `student` `news`",
-                                inline=False)
-                embed.add_field(name="Flaška <:flaska:807228132326899732> ",
-                                value="`flaska`", inline=False)
-                embed.add_field(name="Kahoot <:kahoot:813837093494325278> ", value="`kahoot` ", inline=False)
-                embed.add_field(name="Rank systém :chart_with_upwards_trend: ", value="`rank` `leaderboard`", inline=False)
-                embed.add_field(name="Minecraft server :tent: ", value="`server` ", inline=False)
-                embed.add_field(name=f"Ekonomika {Callouts().emote} ",
-                                value="`daily` `money` `inventory` `hunt` `send` `shop` `kviz` `sell` `prices` ",
-                                inline=False)
-                embed.add_field(name="Hudba :notes: ", value="`join` `leave` `play` `skip` `pause` `resume`",
-                                inline=False)
-                embed.add_field(name="Osu! <:osu:917383973984940072> ", value="`osu` `pp`",
-                                inline=False)
-                embed.set_footer(text="Pro help s jakýmkoli commandem napiš !help [command]")
-                await ctx.send(embed=embed)
-            elif command not in command_names_list:
-                await ctx.send('Tenhle command neexistuje')
-
-    @commands.command(help='Ukáže náhodnou čerstvou zprávu ze světa.', usage='!news')
-    async def news(self, ctx):
+    @app_commands.command(description='Ukáže náhodnou čerstvou zprávu ze světa.')
+    @app_commands.guilds(discord.Object(id=806808047509831700))
+    async def news(self, interaction):
         f = requests.get('https://newsapi.org/v2/top-headlines?country=cz&apiKey={}'.format(os.getenv('NEWS_TOKEN')))
         news = f.json()
-        await ctx.send(news['articles'][random.choice(range(0, len(news['articles'])))]['title'])
-def setup(client):
-    client.add_cog(Funkce(client))
+        await interaction.response.send_message(news['articles'][random.choice(range(0, len(news['articles'])))]['title'])
+
+async def setup(client):
+    await client.add_cog(Funkce(client))
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import discord
 import os
+from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions, Bot
-from kahoot import client
 import requests
-from discord_components import DiscordComponents, Button, Select, SelectOption
+
+import cogs.mainCog
 from callouts import Callouts
 requests.packages.urllib3.disable_warnings()
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!")
-client.remove_command('help')
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(intents=intents, command_prefix='/')
+tree = client.tree
 
 @client.event
 async def on_ready():
     print('Connected to {0.user}'.format(client))
     await client.change_presence(activity= discord.Game(name='Cvičí posilko s Vaňkem | !help'))
-    DiscordComponents(client)
+    await client.load_extension('cogs.mainCog')
+    await tree.sync(guild=discord.Object(id=806808047509831700))
+
+
+@app_commands.command()
+async def fruits(interaction: discord.Interaction, fruit: str):
+    await interaction.response.send_message(f'Your favourite fruit seems to be {fruit}')
+
+
+tree.add_command(fruits, guild=discord.Object(id=806808047509831700))
+
+'''
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
@@ -53,14 +66,8 @@ async def on_command_error(ctx, error):
         print(error)
     else:
         print(error)
-
-@client.command()
-async def button(ctx):
-    await ctx.send('Ahoj', components = [Button(label = 'Pog', custom_id = 'button1', style= 4)])
-    message = await client.wait_for("button_click", check=lambda i: i.custom_id == "button1")
-    await message.send(content= "Button clicked!")
-
-
+'''
+'''
 client.load_extension('cogs.kahoot')
 client.load_extension('cogs.minecraft_server')
 client.load_extension('cogs.economy')
@@ -71,6 +78,8 @@ client.load_extension('cogs.music')
 client.load_extension('cogs.mainCog')
 client.load_extension('cogs.utilities')
 client.load_extension('cogs.osu')
+client.load_extension('cogs.mainCog')
+'''
 
 
 
